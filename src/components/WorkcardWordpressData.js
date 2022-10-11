18a2a647bcf5fcaf6009bc286bba6b4d9e0208ae
Tunji <54449991+Adetunjicode@users.noreply.github.com>
2022-10-12 00:52:26 +0100

    added .jsx extensions to components and added a new wordpress project

--- src/components/WorkcardWordpressData.js
+++ src/components/WorkcardWordpressData.js
@@ -4,38 +4,44 @@ import techblog from "../assets/images/tech-blog.png";
 import comingSoon from "../assets/images/coming-soon.png";
 import tunjiFashionEcommerce from "../assets/images/tunji-fashion-ecommerce-store.png";
 import wordpressPortfolio from "../assets/images/wordpress-portfolio.png";
+import animeWebsite from "../assets/images/anime-website.png";
 
 const ProjectCardData = [
-  {
-    imgsrc: entertainmentblog,
-    title: "An Entertainment Blog",
-    siteUrl: "http://tunji.liveblog365.com/",
-  },
-  {
-    imgsrc: fashionblog,
-    title: "A Fashion Blog",
-    siteUrl: "http://tunji.liveblog365.com/fashion-blog/",
-  },
-  {
-    imgsrc: techblog,
-    title: "A Basic Tech Blog",
-    siteUrl: "http://tunji.liveblog365.com/tech-blog/",
-  },
-  {
-    imgsrc: comingSoon,
-    title: "A Coming Soon Page for clients",
-    siteUrl: "https://tunjiigbatayo.com/coming-soon/",
-  },
-  {
-    imgsrc: tunjiFashionEcommerce,
-    title: "A Fashion Design Ecommerce store",
-    siteUrl: "http://tunji.unaux.com/ecommerce/",
-  },
-  {
-    imgsrc: wordpressPortfolio,
-    title: "My WordPress portfolio website",
-    siteUrl: "http://www.tunji.unaux.com/portfolio/",
-  },
+	{
+		imgsrc: entertainmentblog,
+		title: "An Entertainment Blog",
+		siteUrl: "http://tunji.liveblog365.com/",
+	},
+	{
+		imgsrc: fashionblog,
+		title: "A Fashion Blog",
+		siteUrl: "http://tunji.liveblog365.com/fashion-blog/",
+	},
+	{
+		imgsrc: techblog,
+		title: "A Basic Tech Blog",
+		siteUrl: "http://tunji.liveblog365.com/tech-blog/",
+	},
+	{
+		imgsrc: comingSoon,
+		title: "A Coming Soon Page for clients",
+		siteUrl: "https://tunjiigbatayo.com/coming-soon/",
+	},
+	{
+		imgsrc: tunjiFashionEcommerce,
+		title: "A Fashion Design Ecommerce store",
+		siteUrl: "http://tunji.unaux.com/ecommerce/",
+	},
+	{
+		imgsrc: wordpressPortfolio,
+		title: "My WordPress portfolio website",
+		siteUrl: "http://www.tunji.unaux.com/portfolio/",
+	},
+	{
+		imgsrc: animeWebsite,
+		title: "My WordPress Anime website",
+		siteUrl: "https://www.tunji.com.ng/",
+	},
 ];
 
 export default ProjectCardData;
