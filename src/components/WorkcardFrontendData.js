45a0d9f7f62d7517eb0a9bc00fb96ddf6ef144e9
Adetunji Igbatayo <adetunjiigbatayo@gmail.com>
2022-12-19 16:10:34 +0100

    added payment flow project and image

--- src/components/WorkcardFrontendData.js
+++ src/components/WorkcardFrontendData.js
@@ -6,56 +6,63 @@ import continentalbusters from "../assets/images/continentalbusters.png";
 import wwtbam from "../assets/images/wwtbam.png";
 import reactpagination from "../assets/images/react-pagination.png";
 import nextjspagination from "../assets/images/nextjs-pagination.png";
+import paymentFlow from "../assets/images/payment-flow.png";
 
 const ProjectCardData = [
-	{
-		imgsrc: netflix,
-		title: "ReactJs - Netflix Clone(See Source Code)",
-		siteUrl: "https://netflix-clone-10547.web.app/",
-		githubUrl: "https://github.com/TunjiDev/netflix-clone",
-	},
-	{
-		imgsrc: reactpagination,
-		title: "ReactJs - Basic App with pagination features(See Source Code)",
-		siteUrl: "https://nv221z.csb.app/",
-		githubUrl: "https://github.com/TunjiDev/react-pagination",
-	},
-	{
-		imgsrc: amazon,
-		title: "ReactJs - Ecommerce Website: Amazon Clone(See Source Code)",
-		siteUrl: "https://clone-4af69.web.app/",
-		githubUrl: "https://github.com/TunjiDev/amazon-clone",
-	},
-	{
-		imgsrc: mychat,
-		title: "ReactJs - My-Chat Application(See Source Code)",
-		siteUrl: "https://my-chat-571aa.web.app/",
-		githubUrl: "https://github.com/TunjiDev/my-chat",
-	},
-	{
-		imgsrc: slack,
-		title: "ReactJs - Slack Clone(See Source Code)",
-		siteUrl: "https://slack-clone-15921.firebaseapp.com/",
-		githubUrl: "https://github.com/TunjiDev/slack-clone",
-	},
-	{
-		imgsrc: nextjspagination,
-		title: "NextJs - Basic Fullstack app with pagination features(See source code)",
-		siteUrl: "https://basic-nextjs-fullstack-app.vercel.app/",
-		githubUrl: "https://github.com/TunjiDev/basic-nextjs-fullstack-app",
-	},
-	{
-		imgsrc: continentalbusters,
-		title: "Static Website - Responsive Web Design(See Source Code)",
-		siteUrl: "https://continentalbusters.netlify.app/",
-		githubUrl: "https://github.com/TunjiDev/Continental-Busters",
-	},
-	{
-		imgsrc: wwtbam,
-		title: "VanillaJs - Who Wants To Be a Millonaire Game(See Source Code)",
-		siteUrl: "https://tunjiwwtbam.netlify.app/",
-		githubUrl: "https://github.com/TunjiDev/Who-Wants-To-Be-A-Millonaire-Game-Vanilla-Js-",
-	},
+  {
+    imgsrc: paymentFlow,
+    title: "ReactJs + TypeScript - Payment App(See Source Code)",
+    siteUrl: "https://tjpayment-flow.netlify.app/",
+    githubUrl: "https://github.com/TunjiDev/payment-flow",
+  },
+  {
+    imgsrc: netflix,
+    title: "ReactJs - Netflix Clone(See Source Code)",
+    siteUrl: "https://netflix-clone-10547.web.app/",
+    githubUrl: "https://github.com/TunjiDev/netflix-clone",
+  },
+  {
+    imgsrc: reactpagination,
+    title: "ReactJs - Basic App with pagination features(See Source Code)",
+    siteUrl: "https://nv221z.csb.app/",
+    githubUrl: "https://github.com/TunjiDev/react-pagination",
+  },
+  {
+    imgsrc: amazon,
+    title: "ReactJs - Ecommerce Website: Amazon Clone(See Source Code)",
+    siteUrl: "https://clone-4af69.web.app/",
+    githubUrl: "https://github.com/TunjiDev/amazon-clone",
+  },
+  {
+    imgsrc: mychat,
+    title: "ReactJs - My-Chat Application(See Source Code)",
+    siteUrl: "https://my-chat-571aa.web.app/",
+    githubUrl: "https://github.com/TunjiDev/my-chat",
+  },
+  {
+    imgsrc: slack,
+    title: "ReactJs - Slack Clone(See Source Code)",
+    siteUrl: "https://slack-clone-15921.firebaseapp.com/",
+    githubUrl: "https://github.com/TunjiDev/slack-clone",
+  },
+  {
+    imgsrc: nextjspagination,
+    title: "NextJs - Basic Fullstack app with pagination features(See source code)",
+    siteUrl: "https://basic-nextjs-fullstack-app.vercel.app/",
+    githubUrl: "https://github.com/TunjiDev/basic-nextjs-fullstack-app",
+  },
+  {
+    imgsrc: continentalbusters,
+    title: "Static Website - Responsive Web Design(See Source Code)",
+    siteUrl: "https://continentalbusters.netlify.app/",
+    githubUrl: "https://github.com/TunjiDev/Continental-Busters",
+  },
+  {
+    imgsrc: wwtbam,
+    title: "VanillaJs - Who Wants To Be a Millonaire Game(See Source Code)",
+    siteUrl: "https://tunjiwwtbam.netlify.app/",
+    githubUrl: "https://github.com/TunjiDev/Who-Wants-To-Be-A-Millonaire-Game-Vanilla-Js-",
+  },
 ];
 
 export default ProjectCardData;
